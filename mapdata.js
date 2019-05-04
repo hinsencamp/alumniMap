dd0a44859ebaf82e235489ab7eafb7141c77e4c8
Fabian Hinsenkamp <fabian.hinsenkamp@online.de>
2019-05-04 13:51:33 +0200

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,133 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    DEU1572: {
+      name: "Nordrhein-Westfalen"
+    },
+    DEU1573: {
+      name: "Baden-Württemberg"
+    },
+    DEU1574: {
+      name: "Hessen"
+    },
+    DEU1575: {
+      name: "Bremen"
+    },
+    DEU1576: {
+      name: "Niedersachsen"
+    },
+    DEU1577: {
+      name: "Thüringen"
+    },
+    DEU1578: {
+      name: "Hamburg"
+    },
+    DEU1579: {
+      name: "Schleswig-Holstein"
+    },
+    DEU1580: {
+      name: "Rheinland-Pfalz"
+    },
+    DEU1581: {
+      name: "Saarland"
+    },
+    DEU1591: {
+      name: "Bayern"
+    },
+    DEU1599: {
+      name: "Berlin"
+    },
+    DEU1600: {
+      name: "Sachsen-Anhalt"
+    },
+    DEU1601: {
+      name: "Sachsen"
+    },
+    DEU3487: {
+      name: "Brandenburg"
+    },
+    DEU3488: {
+      name: "Mecklenburg-Vorpommern"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "52.516667",
+      lng: "13.4",
+      name: "Berlin"
+    }
+  },
+  labels: {},
+  regions: {}
+};
